717f92159f98b4ee266267727e7d90dd0557cf76
zulkifli <izqfly@gmail.com>
2022-04-07 00:21:56 +0700

    update menu tambah crud tanah (KIB A)

---
+++ resources/views/admin/master/barang/tanah.blade.php
@@ -0,0 +1,381 @@
+@extends('admin.layout.index')
+
+@section('title') Tanah (KIB A) @endsection
+@section('head')
+<link rel="stylesheet" type="text/css" href="{{ asset('assets/vendor/datatables.net-bs4/css/dataTables.bootstrap4.min.css') }}">
+
+<link rel="stylesheet" type="text/css" href="{{ asset('assets/vendor/data-table/extensions/responsive/css/responsive.dataTables.css') }}">
+<link rel="stylesheet" href="{{ asset('assets/vendor/chosen_v1.8.7/chosen.css') }}">
+<link rel="stylesheet" href="https://js.arcgis.com/4.17/esri/themes/light/main.css">
+
+<style>
+    table.table-bordered tbody td {
+        word-break: break-word;
+        vertical-align: top;
+    }
+</style>
+@endsection
+
+@section('page-header')
+
+<div class="row align-items-end">
+    <div class="col-lg-8">
+        <div class="page-header-title">
+            <div class="d-inline">
+                <h4>Tanah (KIB A)</h4>
+                <span></span>
+            </div>
+        </div>
+    </div>
+    <div class="col-lg-4">
+        <div class="page-header-breadcrumb">
+            <ul class="breadcrumb-title">
+                <li class="breadcrumb-item">
+                    <a href="index-1.htm"> <i class="feather icon-home"></i> </a>
+                </li>
+                <li class="breadcrumb-item"><a href="#!">Unit organisasi</a>
+                </li>
+                <li class="breadcrumb-item"><a href="#">Unit</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</div>
+@endsection
+
+@section('filter')
+<div class="card">
+    <div class="accordion-panel">
+        <div class="accordion-heading" role="tab" id="headingOne">
+            <h3 class="card-title accordion-title">
+                <a class="accordion-msg" data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                <i class="icofont icofont-ui-search"></i>  Pencarian Data
+                </a>
+            </h3>
+        </div>
+        <div id="collapseOne" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="headingOne">
+            <div class="accordion-content accordion-desc">
+                <form action="{{route('getUnitFilter')}}" method="post">
+                    @csrf
+                    <div class="modal-body">
+                        <div class="row">
+                            <div class="col-sm-6">
+                                <div class="form-group row">
+                                    <label class="col-md-3 col-form-label">Bidang</label>
+                                    <div class="col-md-9">
+                                         
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+                                    <label class="col-md-3 col-form-label">Nama Unit</label>
+                                    <div class="col-md-9">
+                                        <input name="nama_unit" id="edit_nama_unit" value=" " type="text" class="form-control"  >
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-sm-6">
+                                <div class="form-group row">
+                                    <label class="col-md-3 col-form-label">Kode Unit</label>
+                                    <div class="col-md-9">
+                                        <input name="kode_unit" type="number" value=" " id="edit_kode_unit" class="form-control"  >
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="submit"  class="btn btn-sm btn-round btn-primary mb-3"><i class="icofont icofont-ui-search"></i> Cari</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+
+@endsection
+
+@section('page-body')
+<div class="row">
+    <div class="col-sm-12">
+        <div class="card">
+            <div class="card-block">
+                @if (hasAccess(Auth::user()->role_id, "Unit", "Create"))
+                <a data-toggle="modal" href="#addModal" class="btn btn-sm btn-round btn-primary mb-3"><i class="icofont icofont-plus-circle"></i> Tambah Tanah (KIB A)</a>
+                @endif
+                <div class="dt-responsive table-responsive">
+                    <table id="dttable" class="table table-striped table-bordered able-responsive">
+                        <thead>
+                            <tr>
+                                <th>No</th>
+                                <th>Tahun</th>
+                                <th>Kode Pemilik</th>
+                                <th>Kode Aset</th>
+                                <th>No Register</th>
+                                <th>Tgl Pembelian</th>
+                                <th>Tgl Pembukuan</th>
+                                <th>Luas (m2)</th>
+                                <th>Alamat</th>
+                                <th>Hak Tanah</th>
+                                <th>Tgl Sertifikat</th>
+                                <th>No Sertifikat</th>
+                                <th>Asal Usul</th>
+                                <th>Penggunaan</th>
+                                <th>Harga</th>
+                                <!-- <th>Foto</th> -->
+                                <th style="min-width: 100px;">Aksi</th>
+                            </tr>
+                        </thead>
+                        <tbody id="bodyTanah">
+
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="modal-only">
+    @if (hasAccess(Auth::user()->role_id, "Unit", "Create"))
+    <div class="modal fade" id="addModal" tabindex="-1" role="dialog">
+        <div class="modal-dialog modal-lg" role="document">
+            <div class="modal-content">
+                <form action="{{route('saveUnit')}}" method="post">
+                    @csrf
+                    <div class="modal-header">
+                        <h4 class="modal-title">Tambah Data Unit</h4>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        <div class="form-group row">
+                            <label class="col-md-3 col-form-label">Bidang</label>
+                            <div class="col-md-9">
+                                 
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label class="col-md-3 col-form-label">Kode Unit</label>
+                            <div class="col-md-9">
+                                <input name="kode_unit" type="number" class="form-control" required>
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label class="col-md-3 col-form-label">Nama Unit</label>
+                            <div class="col-md-9">
+                                <input name="nama_unit" type="text" class="form-control" required>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-default btn-sm btn-raund  waves-effect " data-dismiss="modal">Tutup</button>
+                        <button type="submit" class="btn btn-primary btn-sm btn-raund waves-effect waves-light ">Simpan</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+    @endif
+
+    @if (hasAccess(Auth::user()->role_id, "Unit", "Update"))
+    <div class="modal fade" id="editModal" tabindex="-1" role="dialog">
+        <div class="modal-dialog modal-lg" role="document">
+            <div class="modal-content">
+                <form action="{{route('updateUnit')}}" method="post">
+                    @csrf
+                    <div class="modal-header">
+                        <h4 class="modal-title">Ubah Data Unit</h4>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                        <input type="hidden" id="unit_id" name="unit_id" />
+                    </div>
+                    <div class="modal-body">
+                        <div class="form-group row">
+                            <label class="col-md-3 col-form-label">Bidang</label>
+                            <div class="col-md-9">
+                                 
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label class="col-md-3 col-form-label">Kode Unit</label>
+                            <div class="col-md-9">
+                                <input name="kode_unit" type="number" id="kode_unit2" class="form-control" required>
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label class="col-md-3 col-form-label">Nama Unit</label>
+                            <div class="col-md-9">
+                                <input name="nama_unit" id="nama_unit2" type="text" class="form-control" required>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-default btn-sm btn-raund  waves-effect " data-dismiss="modal">Tutup</button>
+                        <button type="submit" class="btn btn-primary btn-sm btn-raund waves-effect waves-light ">Simpan</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+    @endif
+
+    @if (hasAccess(Auth::user()->role_id, "Unit", "Delete"))
+    <div class="modal fade" id="delModal" tabindex="-1" role="dialog">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h4 class="modal-title">Konfirmasi Hapus Data </h4>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <p>Apakah anda yakin ingin menghapus data ini?</p>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-default btn-sm waves-effect " data-dismiss="modal">Tutup</button>
+                    <a id="delHref" href="" class="btn btn-danger  btn-sm waves-effect waves-light ">Hapus</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    @endif
+</div>
+@endsection
+@section('script')
+
+<script src="{{ asset('assets/vendor/datatables.net/js/jquery.dataTables.min.js') }}"></script>
+<script src="{{ asset('assets/vendor/datatables.net/js/dataTables.buttons.min.js') }}"></script>
+<script src="{{ asset('assets/vendor/datatables.net/js/dataTables.bootstrap4.min.js') }}"></script>
+
+<script src="{{ asset('assets/vendor/data-table/extensions/responsive/js/dataTables.responsive.min.js') }}"></script>
+<script src="{{ asset('assets/vendor/data-table/extensions/responsive/js/responsive.bootstrap4.min.js') }}"></script>
+<script src="{{ asset('assets/vendor/jquery/js/jquery.mask.js') }}"></script>
+<script type="text/javascript" src="{{ asset('assets/vendor/chosen_v1.8.7/chosen.jquery.js') }}" type="text/javascript"></script>
+
+<script>
+    $(document).ready(function() {
+        $(".chosen-select").chosen({
+            width: '100%'
+        });
+        var table = $('#dttable').DataTable({
+            processing: true,
+            serverSide: true,
+            bFilter: false,
+            ajax: {
+                url: "{{ url('admin/master-data/barang/intra/tanah/json') }}",
+               
+            },
+
+            columns: [{
+                    'mRender': function(data, type, full, meta) {
+                        return +meta.row + meta.settings._iDisplayStart + 1;
+                    }
+                },
+                {
+                    data: 'tahun',
+                    name: 'tahun'
+                },
+                {
+                    data: 'kd_pemilik',
+                    name: 'kd_pemilik'
+                },
+                {
+                    data: 'kode_aset',
+                    name: 'kode Aset'
+                },
+                {
+                    data: 'no_register',
+                    name: 'no_register'
+                },
+                {
+                    data: 'tgl_perolehan',
+                    name: 'tgl_pembelian'
+                },
+                {
+                    data: 'tgl_pembukuan',
+                    name: 'tgl_pembukuan'
+                },
+                {
+                    data: 'luas_m2',
+                    name: 'luas_m2'
+                },
+                {
+                    data: 'alamat',
+                    name: 'alamat'
+                },
+                {
+                    data: 'hak_tanah',
+                    name: 'hak_tanah'
+                },
+                {
+                    data: 'sertifikat_tanggal',
+                    name: 'tgl_sertifikat'
+                },
+                {
+                    data: 'sertifikat_nomor',
+                    name: 'no_sertifikat'
+                },
+                {
+                    data: 'asal_usul',
+                    name: 'asal_usul'
+                }, 
+                {
+                    data: 'penggunaan',
+                    name: 'penggunaan'
+                },
+                {
+                    data: 'harga',
+                    name: 'harga'
+                }, 
+                {
+                    data: 'action',
+                    name: 'action',
+                    orderable: false,
+                    searchable: false
+                },
+            ]
+        });
+    });
+
+
+    @if(hasAccess(Auth::user()->role_id, "Unit", "Update"))
+    $('#editModal').on('show.bs.modal', function(event) {
+        const link = $(event.relatedTarget);
+        const id = link.data('id');
+        console.log(id);
+        const baseUrl = `{{ url('admin/master-data/unit-organisasi/unit/getUnitById/') }}` + '/' + id;
+        $.get(baseUrl,
+            function(response) {
+                const data = response.data;
+                showData(data);
+            });
+    });
+
+    function showData(data) {
+        $(".chosen-select").val(data.kode_bidang).trigger('chosen:updated');
+        $("#kode_unit2").val(data.kode_unit);
+        $("#nama_unit2").val(data.nama_unit);
+        $("#unit_id").val(data.id);
+    }
+
+    @endif
+    @if(hasAccess(Auth::user()->role_id, "Unit", "Delete"))
+    $('#delModal').on('show.bs.modal', function(event) {
+        const link = $(event.relatedTarget);
+        const id = link.data('id');
+        console.log(id);
+        const url = `{{ url('admin/master-data/unit-organisasi/unit/delete') }}/` + id;
+        console.log(url);
+        const modal = $(this);
+        modal.find('.modal-footer #delHref').attr('href', url);
+    });
+    @endif
+</script>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -115,6 +115,14 @@ Route::group(['prefix' => 'admin', 'middleware' => ['auth']], function () {
     });
 
     Route::group(['prefix' => 'master-data'], function () {
+        Route::group(['prefix' => 'barang'], function () {
+            Route::group(['prefix' => 'intra'], function () {
+                Route::get('tanah', 'MasterData\Barang\TanahController@index')->name('getTanah');
+                Route::get('tanah/json', 'MasterData\Barang\TanahController@json')->name('getJsonTanah');
+                Route::get('tanah/detail/{id}', 'MasterData\Barang\TanahController@detail')->name('getDetailKIBA');
+            });
+        });
+
         Route::group(['prefix' => 'unit-organisasi'], function () {
             Route::get('bidang', 'MasterData\BidangController@index')->name('getBidang');
             Route::get('bidang/json', 'MasterData\BidangController@json')->name('getJsonBidang');
